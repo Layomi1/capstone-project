251598c34e15a94365ad5020e3c5fef6dbb41bb6
olufunke1011 <owoseniolufunke1011@gmail.com>
2024-04-27 14:32:17 +0100

    corrected the input boxes to display text when typed

--- src/Pages/Donate/Donate.jsx
+++ src/Pages/Donate/Donate.jsx
@@ -1,7 +1,85 @@
+import ImageUpload from "../../components/ImageUpload/ImageUpload"
+import CustomButton from "../../components/CustomButton/CustomButton"
+import style from "../Donate/Donate.module.css"
+import DonateImg from "../../assets/donate.svg"
+import { Link } from "react-router-dom";
 const Donate = () => {
   return (
-    <div>
-      <h1>Donate</h1>
+    <div className={style.donateContainer}>
+      <div className={style.donateTitleContainer}>
+            <div className={style.donateTitleParaContainer}>
+                <h3 className={style.donateTitle}>Welcome To Donation Centre</h3>
+                <p className={style.titlePara}>Spread Kindness</p>
+                <p className={style.titlePara}>Donate items you no longer NEED to those who NEED</p> 
+            </div>
+            <div>
+                  <img src={DonateImg} alt="hands with a heart" className={style.donateImage} />
+            </div>    
+        </div>
+        <div className={style.sellersFormContainer}>
+            <form action="" className={style.sellersForm}>
+              <label className={style.labelTitle}>Name/Title of item</label><br/>
+              <input type="text" className={style.inputTypes}/><br/><br/>
+
+                <label className={style.labelTitle}>Product Description</label><br/>
+                <textarea className={style.productDescription}></textarea><br/><br/>
+
+                <label className={style.labelTitle}>Item Location</label><br/>
+                <input type="text" className={style.inputTypes}/><br/><br/>
+
+                <label className={style.labelTitle}>Year of Purchase</label><br/>
+                <input type="number" className={style.inputTypes}/><br/><br/>
+
+                <label className={style.labelTitle}>Phone Number</label><br/>
+                <input type="phone" className={style.inputTypes}/><br/><br/>
+               
+                <div className={style.receiptUploadContainer}>
+                    <label className={style.receiptTitle}>Purchase Receipt</label>
+                    <ImageUpload/>
+                </div>
+                <div>
+                    <div className={style.selection}>Category
+                    <select className={style.itemSelection}>
+                      <option value=""></option>
+                      <option value=""></option>
+                    </select>
+                    </div>
+
+                    <div className={style.selection}>Condition
+                    <select className={style.itemSelection}>
+                      <option value=""></option>
+                      <option value=""></option>
+                    </select>
+                    </div>
+
+                    <div className={style.selection}>Missing Part
+                    <select className={`${style.itemSelection} ${style.missingPart}`}>
+                      <option value=""></option>
+                      <option value=""></option>
+                    </select>
+                    </div>
+                    <input type="text" placeholder="if yes, state the missing part(s)" className={style.missigPartInput} />
+
+                    <div className={style.uploadImageContainer}>
+                        <div className={style.uploadImage}>
+                          <h4>Add Photos</h4>
+                          <p>Add minimun of 2 photos and maximum of 5. First photo will be on the front page</p>
+                        </div>
+                        <div className={style.imageUploadIcons}>
+                           <ImageUpload/>
+                           <ImageUpload/>
+                           <ImageUpload/>
+                           <ImageUpload/>
+                           <ImageUpload/>
+                        </div>
+                    </div>
+                   
+                </div><br/><br/>
+               
+               
+            </form>
+        </div>
+
     </div>
   );
 };
